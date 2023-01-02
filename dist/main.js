ba94233134f91942a7b3a38bc74ef74e62a68138
Thaddaeus Fam <103373668+cColds@users.noreply.github.com>
2023-01-02 08:01:41 -0600

    Add progress bar

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,12 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
+const progressPercentage = document.querySelector(".progress-percentage");
+
+// setInterval(() => {
+// 	console.log(progressPercentage.clientWidth);
+// }, 5000);
 
 /******/ })()
 ;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBOztBQUVBO0FBQ0E7QUFDQSxJQUFJIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vaW1hZ2UtY2Fyb3VzZWwvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgcHJvZ3Jlc3NQZXJjZW50YWdlID0gZG9jdW1lbnQucXVlcnlTZWxlY3RvcihcIi5wcm9ncmVzcy1wZXJjZW50YWdlXCIpO1xuXG4vLyBzZXRJbnRlcnZhbCgoKSA9PiB7XG4vLyBcdGNvbnNvbGUubG9nKHByb2dyZXNzUGVyY2VudGFnZS5jbGllbnRXaWR0aCk7XG4vLyB9LCA1MDAwKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const progressPercentage = document.querySelector(".progress-percentage");
+
+// setInterval(() => {
+// 	console.log(progressPercentage.clientWidth);
+// }, 5000);
