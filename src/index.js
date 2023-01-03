e1d8440fbb7f540ca96858fcdda56c517f605def
Thaddaeus Fam <103373668+cColds@users.noreply.github.com>
2023-01-03 08:09:37 -0600

    Slide dots can change image

--- src/index.js
+++ src/index.js
@@ -7,9 +7,68 @@ function hideCurrentImage() {
 	getCurrentImage().classList.remove("selected");
 }
 
-function displayNewImage(newImage) {
+function unselectCurrentDot() {
+	const currentDot = document.querySelector(".slide-dots .selected");
+	currentDot.classList.remove("selected");
+}
+
+function selectNewDot(e) {
+	e.target.closest("li").classList.add("selected");
+}
+
+function displayNewImage(imageIndex) {
+	const newImage = document.querySelector(
+		`[data-image-index='${imageIndex}']`
+	);
 	newImage.classList.add("selected");
 }
+
+function handleLeftArrow() {
+	const images = document.querySelectorAll(".image-frame li");
+
+	const currentImageIndex = +getCurrentImage().dataset.imageIndex;
+	const lastImageIndex = images.length - 1;
+
+	hideCurrentImage();
+
+	if (currentImageIndex === 0) {
+		displayNewImage(lastImageIndex);
+	} else {
+		displayNewImage(currentImageIndex - 1);
+	}
+}
+
+function handleRightArrow() {
+	const images = document.querySelectorAll(".image-frame li");
+
+	const currentImageIndex = +getCurrentImage().dataset.imageIndex;
+	const lastImageIndex = images.length - 1;
+
+	hideCurrentImage();
+
+	if (currentImageIndex === lastImageIndex) {
+		displayNewImage(0);
+	} else {
+		displayNewImage(currentImageIndex + 1);
+	}
+}
+
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+const slideDots = document.querySelector(".slide-dots");
+
+leftArrow.addEventListener("click", handleLeftArrow);
+rightArrow.addEventListener("click", handleRightArrow);
+slideDots.addEventListener("click", (e) => {
+	if (e.target.className === "slide-dots") return;
+
+	const dotSelectedIndex = +e.target.closest("li").dataset.dotIndex;
+
+	unselectCurrentDot();
+	selectNewDot(e);
+	hideCurrentImage();
+	displayNewImage(dotSelectedIndex);
+});
 
 // function handleImageChange() {
 //     const images = document.querySelectorAll(".image-frame li");
@@ -26,39 +85,3 @@ function displayNewImage(newImage) {
 // 	}
 
 // }
-
-function handleLeftArrow() {
-	const images = document.querySelectorAll(".image-frame li");
-
-	const currentImageIndex = +getCurrentImage().dataset.imageIndex;
-	const lastImageIndex = images.length - 1;
-
-	hideCurrentImage();
-
-	if (currentImageIndex === 0) {
-		displayNewImage(images[lastImageIndex]);
-	} else {
-		displayNewImage(images[currentImageIndex - 1]);
-	}
-}
-
-function handleRightArrow() {
-	const images = document.querySelectorAll(".image-frame li");
-
-	const currentImageIndex = +getCurrentImage().dataset.imageIndex;
-	const lastImageIndex = images.length - 1;
-
-	hideCurrentImage();
-
-	if (currentImageIndex === lastImageIndex) {
-		displayNewImage(images[0]);
-	} else {
-		displayNewImage(images[currentImageIndex + 1]);
-	}
-}
-
-const leftArrow = document.querySelector(".left-arrow");
-const rightArrow = document.querySelector(".right-arrow");
-
-leftArrow.addEventListener("click", handleLeftArrow);
-rightArrow.addEventListener("click", handleRightArrow);
